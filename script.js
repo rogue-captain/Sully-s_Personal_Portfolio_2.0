cff883c2332b6a4e247c8d7f13611505183dbc33
Sully Erkan-Rijos <mr.suleyman.erkan@gmail.com>
2025-03-31 22:12:49 -0400

    meep

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function startSite() {
+	        window.scrollTo(0, 0);
+    
+        var header = document.getElementById("sullys-welcome");
+
+        header.classList.add("slideIn");
+}
+
+
+
+    window.onload = function() {
+		startSite();
+    } ;
