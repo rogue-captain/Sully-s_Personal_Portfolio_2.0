4cfb09b4e979f68ac35f545c98e0961dea98d384
Sully Erkan-Rijos <mr.suleyman.erkan@gmail.com>
2025-04-27 18:22:56 -0400

    modded media queries in css for menu display and JS added function for menu icon toggle / display

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 
 function startSite() {
-	        window.scrollTo(0, 0);
+	
+	console.log("startSite function called.") //DEBUGGING LINE
+	
+	window.scrollTo(0, 0);
     
-        var header = document.getElementById("sullys-welcome");
+    var header = document.getElementById("sullys-welcome");
 
-        header.classList.add("slideIn");
+    header.classList.add("slideIn");
+}
+
+
+
+function toggleNavMenuIcon () {
+	
+	console.log("box menu icon function called.") //DEBUGGING LINE
+	
+	let menuIcon = document.getElementById('boxMenuIcon')
+	let navBar = document.getElementById('navbar')
+	
+		menuIcon.onclick = function () {
+			
+			menuIcon.classList.toggle('bx-x');
+			navBar.classList.toggle('nav-active');
+		};
 }
 
 
@@ -16,4 +35,5 @@ function portfolioCarousel() {}
 
     window.onload = function() {
 		startSite();
+		toggleNavMenuIcon();
     } ;
